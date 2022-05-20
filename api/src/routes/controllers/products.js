58b77000520d7932be670b1c71c2be00a753813c
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-05-20 09:49:00 -0500

    filter by name and category from back

--- api/src/routes/controllers/products.js
+++ api/src/routes/controllers/products.js
@@ -15,69 +15,45 @@ module.exports = {
         return await Product.findAndCountAll(query);
       };
 
-      let query;
+      const query = {
+        where: {
+          state: "Available",
+        },
+        attributes: [
+          "id_product",
+          "serie",
+          "name",
+          "description",
+          "technique",
+          "measures",
+          "image",
+          "price",
+          "sku",
+          "released",
+          "state",
+        ],
+        limit: 3,
+        offset: page * 3,
+        include: [
+          {
+            model: Category,
+            attributes: ["id_category", "name"],
+            through: {
+              attributes: [],
+            },
+          },
+        ],
+      };
       if (req.query.name) {
         const { name } = req.query;
-        query = {
-          where: {
-            name: {
-              [Op.like]: `%${name.toLowerCase()}%`,
-            },
-          },
-          attributes: [
-            "id_product",
-            "serie",
-            "name",
-            "description",
-            "technique",
-            "measures",
-            "image",
-            "price",
-            "sku",
-            "released",
-            "state",
-          ],
-          limit: 20,
-          offset: page * 20,
-          include: [
-            {
-              model: Category,
-              attributes: ["id_category", "name"],
-              through: {
-                attributes: [],
-              },
-            },
-          ],
+        query.where.name = {
+          [Op.iLike]: `%${name.toLowerCase()}%`,
         };
-      } else {
-        query = {
-          where: {
-            state: "Available",
-          },
-          attributes: [
-            "id_product",
-            "serie",
-            "name",
-            "description",
-            "technique",
-            "measures",
-            "image",
-            "price",
-            "sku",
-            "released",
-            "state",
-          ],
-          limit: 3,
-          offset: page * 3,
-          include: [
-            {
-              model: Category,
-              attributes: ["id_category", "name"],
-              through: {
-                attributes: [],
-              },
-            },
-          ],
+      }
+      if (req.query.category) {
+        const { category } = req.query;
+        query.include[0].where = {
+          id_category: category,
         };
       }
       const products = await getProducts(query);
